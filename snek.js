1a9573820a2a146badf4ec6936807df254c7a46c
sanskriti-sahu <87552267+sanskriti-sahu@users.noreply.github.com>
2021-08-06 23:13:14 -0700

    organizing the code

--- snek.js
+++ snek.js
@@ -1,61 +1,5 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var game = {
-    tickNumber: 0,
-    board: [
-        "####################",
-        "#                  #",
-        "#                  #",
-        "#       ####       #",
-        "#       ####       #",
-        "#                  #",
-        "#                  #",
-        "#                  #",
-        "####################"
-    ],
-    tick: function() {}
-};
-
-
-
-var graphics = {
-    canvas: document.getElementById("canvas"),
-    squareSize: 30,
-    drawBoard: function() {
-        var ctx = graphics.canvas.getContext("2d");
-        var currentY = 0;
-        game.board.forEach(function checkLine(line) {
-            line = line.split('');
-            var currentX = 0;
-            line.forEach(function checkCharac(character) {
-                if (character == '#') {
-                    ctx.fillStyle = "black";
-                    ctx.fillRect(currentX, currentY, graphics.squareSize, graphics.squareSize);
-                }
-
-                currentX = currentX + graphics.squareSize;
-            });
-            currentY = currentY + graphics.squareSize;
-        });
-    }
-};
-
-graphics.drawBoard();
-var gameControl = {
-    startGame: function() {
-        game.tick();
-    }
-};
-gameControl.startGame();
-
-tick: function ticksFun() {
-    game.tickNumber++;
-    graphics.drawGame();
-    game.timer = window.setTimeout("game.ticks()", 500);
-}
-
-
 var snake = {
+    squareSize: 25,
     parts: [{
             x: 3,
             y: 2
@@ -71,15 +15,74 @@ var snake = {
     facing: "E",
     color: "green",
     canvas: document.getElementById("canvas"),
-    draw: function() {
-        var canvas = document.getElementById("canvas");
-        var ctx = graphics.canvas.getContext("2d");
+    draw: function(ctx) {
         snake.parts.forEach(function drawPart(part) {
-            var xLocation = part.x * graphics.squareSize;
-            var yLocation = part.y * graphics.squareSize;
+            var xLocation = part.x * snake.squareSize;
+            var yLocation = part.y * snake.squareSize;
             ctx.fillStyle = snake.color;
-            ctx.fillRect(xLocation, yLocation, graphics.squareSize, graphics.squareSize);
+            ctx.fillRect(xLocation, yLocation, snake.squareSize, snake.squareSize);
         });
+    },
+    move: function() {
+
     }
 }
-snake.draw();
+
+var graphics = {
+    canvas: document.getElementById("canvas"),
+    squareSize: 30,
+    drawBoard: function(ctx) {
+        //var ctx = graphics.canvas.getContext("2d");
+        var currentY = 0;
+        game.board.forEach(function checkLine(line) {
+            line = line.split('');
+            var currentX = 0;
+            line.forEach(function checkCharac(character) {
+                if (character == '#') {
+                    ctx.fillStyle = "black";
+                    ctx.fillRect(currentX, currentY, graphics.squareSize, graphics.squareSize);
+                }
+
+                currentX = currentX + graphics.squareSize;
+            });
+            currentY = currentY + graphics.squareSize;
+        });
+    },
+
+    drawGame: function() {
+        var ctx = graphics.canvas.getContext("2d");
+        graphics.drawBoard(ctx);
+        snake.draw(ctx);
+    }
+};
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var game = {
+    tickNumber: 0,
+    timer: null,
+    board: [
+        "####################",
+        "#                  #",
+        "#                  #",
+        "#       ####       #",
+        "#       ####       #",
+        "#                  #",
+        "#                  #",
+        "#                  #",
+        "####################"
+    ],
+    tick: function() {
+        game.tickNumber++;
+        graphics.drawGame();
+        console.log(game.tickNumber);
+        game.timer = window.setTimeout("game.tick()", 500);
+    }
+};
+//graphics.drawBoard();
+var gameControl = {
+    startGame: function() {
+        game.tick();
+    }
+};
+gameControl.startGame();
